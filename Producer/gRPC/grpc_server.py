c2f2032ddc270ab7e82b6288a394da5565c12f7f
Eli Harper <eli.harper@caci.com>
2020-09-01 12:46:08 -0400

    Initial commit.

---
+++ Producer/gRPC/grpc_server.py
@@ -0,0 +1,52 @@
+""" Implementation of my gRPC executor server.  """
+
+from concurrent import futures
+
+import grpc
+import logging
+from google.protobuf.json_format import MessageToJson, MessageToDict
+
+import cmf_pb2
+import cmf_pb2_grpc
+
+# Global declarations:
+LOGGER_NAME = 'server_logger'
+LOG_LOCATION = 'log/gRPC_Server.log'
+LOG_LEVEL = logging.INFO
+logger = logging.getLogger(LOGGER_NAME)
+
+class Executor(message_pb2_grpc.ExecutorServicer):
+    def HandleCmfxMsg(self, request, context):
+        messagedict = MessageToDict(request)
+        message = messagedict['contents']
+
+        if message.startswith('Lorem') and message.endswith('Cur'):
+            return cmf_pb2.ChangeReply(message='success')
+        else:
+            return cmf_pb2.ChangeReply(message='Received, but contents incorrect.')
+
+
+def configure_logger() -> logging.Logger:
+    logger = logging.getLogger(LOGGER_NAME)
+    handler = logging.FileHandler(LOG_LOCATION)
+
+    logger.addHandler(handler)
+    logger.setLevel(LOG_LEVEL)
+
+    return logger
+
+
+def serve():
+    global logger
+    
+    logger = configure_logger()
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    cmf_pb2_grpc.add_ExecutorServicer_to_server(Executor(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    logger.info('Started server.')
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
---
+++ __init__.py
@@ -0,0 +1,5 @@
+import pb.cmf_pb2
+import pb.cmf_pb2_grpc
+
+import Consumer.gRPC.grpc_client
+import Producer.gRPC.grpc_server
