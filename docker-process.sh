f4b0de6f52ddfcea9da73f27104da966090ed188
Eli Harper <eli.harper1995@gmail.com>
2020-09-11 14:25:24 -0400

    Added a docker-process script that builds & tags the four Python modules of this test suite. I also wrote the 4 Dockerfiles required for this. Now to run this in the test environment and add revisions to abstract the preparation & testing portions to a common, globally accessible file.

---
+++ docker-process.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+
+# Hoistied functions:
+function getSub {
+     local consprod=$1
+     if [[ $consprod == *"consumer"* ]]; then
+	 SUB="Consumers"
+     else
+	 SUB="Producers"
+     fi
+}	     
+
+function getFmt {
+    local fmt=$1
+    if [[ $fmt == *"grpc"* ]]; then
+	FMT="gRPC"
+    else
+	FMT="Kafka"
+    fi
+}
+# End of hoisties
+
+
+VERSION=$1
+
+if (( $# > 1)); then
+    # Only perform on the provided images:
+    for i in ${@:2}
+    do
+	 i=${i//,}
+	 echo "Running for ${i}"
+         getSub "$i"
+	 getFmt "$i"	 
+    	 docker build -f Dockerfiles/$SUB/$FMT/Dockerfile -t $i . && \
+	 docker tag $i eliharper/$i:$VERSION && \
+	 docker push eliharper/$i:$VERSION
+    done
+    exit 0
+fi
+
+images=("grpc_consumer", "grpc_producer", "kafka_consumer", "kafka_producer")
+echo "RUNNING FOR ALL!"
+
+for i in "${images[@]}"
+do
+         i=${i//,}
+         getSub $i
+	 getFmt $i
+	 docker build -f Dockerfiles/$SUB/$FMT/Dockerfile -t ${i//,} . && \
+	 docker tag $i eliharper/$i:$VERSION && \
+	 docker push eliharper/$i:$VERSION
+	 
+done
+
